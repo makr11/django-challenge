de55c47cca0e8c364f01b6aa51ab2183a2da6b1e
marin <marin.kruzic@gmail.com>
2022-06-28 01:21:06 +0200

    Initial commit
    Created docker compose file which starts necessary services.
    Created initial Django and required product model migration.

---
+++ shop/app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from decimal import Decimal
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class GreatProduct(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=2,
+                                 validators=[MinValueValidator(Decimal('0.00')), MaxValueValidator(Decimal('5.00'))])
+    price = models.DecimalField(max_digits=10, decimal_places=2,
+                                validators=[MinValueValidator(Decimal('0.00')), MaxValueValidator(Decimal('99999999.99'))])
+    updated_at = models.DateTimeField(blank=True)
